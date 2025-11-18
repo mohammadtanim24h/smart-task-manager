011ae32fedb5a6d5983a513a78f5e1e10ab1ac92
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2025-11-18 08:43:47 +0600

    setup shadcn ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button"
 function App() {
   return (
     <>
       <p className="text-3xl font-bold underline">Hello World</p>
+      <Button>Click me</Button>
     </>
   )
 }
